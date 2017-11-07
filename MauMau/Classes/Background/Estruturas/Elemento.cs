4be9ea32d972459e6e9fa5d1cb59d219c28abdb3
Lucas Gomes <lucasgsm88@gmail.com>
2017-11-06 23:56:42 -0200

    fixing Lista and Pilha

--- MauMau/Classes/Background/Estruturas/Elemento.cs
+++ MauMau/Classes/Background/Estruturas/Elemento.cs
@@ -8,30 +8,32 @@ namespace MauMau.Classes.Background.Estruturas
 {
     class Elemento
     {
-        private object dado;
+        private Dado dado;
         private Elemento proximo;
         private int index;
 
         public Elemento Prox { get { return this.proximo; } set { this.proximo = value; } }
 
-        public Elemento(object dado)
-        {
-            this.dado = dado;
-        }
         public Elemento(object dado, int index)
         {
-            this.dado = dado;
+            this.dado = new Dado(dado, index);
             this.index = index;
         }
 
-        public object GetDado()
+        public Dado GetDado()
         {
             return this.dado;
         }
-
-        public void SetDado(object dado)
+        
+        public void SetDado(Dado dado)
         {
             this.dado = dado;
+        }
+
+        public void SetDado(object dado, int index)
+        {
+            this.dado.Info = dado;
+            this.dado.Index = index;
         }
     }
 }
