eda992d381f2fd02062bbf0f2d374c82641ccc42
Lucas Gomes <lucasgsm88@gmail.com>
2017-11-10 18:46:01 -0200

    Creating player play

---
+++ MauMau/Classes/Utilitario/Code.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauMau.Classes.Utilitario
+{
+    public static class Code
+    {
+        private static Random randomval = new Random();
+        public static string Generate(int size)
+        {
+            string toreturn = "";
+            for(int i = 0; i < size; i++)
+            {
+                toreturn += randomval.Next(0, 9);
+            }
+            return toreturn;
+        }
+    }
+}
