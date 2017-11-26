feee4a9f29bddc44f02fcce3e2c15d9603ccc335
Lucas Gomes <lucasgsm88@gmail.com>
2017-11-26 18:34:18 -0200

    Send right project

--- MauMau/Classes/Background/Cartas/Especial.cs
+++ MauMau/Classes/Background/Cartas/Especial.cs
@@ -2,7 +2,6 @@
 using System.Windows.Media;
 using MauMau.Classes.Background.Enum;
 using MauMau.Classes.Background.Interfaces;
-
 namespace MauMau.Classes.Background.Cartas
 {
     class Especial : Carta
--- MauMau/Classes/Background/Log.cs
+++ MauMau/Classes/Background/Log.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MauMau.Classes.Background.Cartas;
 
 namespace MauMau.Classes.Background
 {
@@ -11,44 +10,14 @@ namespace MauMau.Classes.Background
     {
         static List<String> listaEventos = new List<string>();
 
-        public static List<String> ListaEventos { get => listaEventos; set => listaEventos = value; }
+        public static List<string> ListaEventos { get => listaEventos; set => listaEventos = value; }
 
         /// <summary>
-        /// Chamado sempre que uma carta for jogada.
+        /// Método que será chamado sempre que um novo evento acontecer durante o jogo.
         /// </summary>
-        static public void AdicionarEvento(Object obj)
+        static public void AdicionarEvento(string jogadorAtual, string cartaJogada, DateTime tempoJogada)
         {
-            string horaAtual = DateTime.UtcNow.TimeOfDay.ToString().Substring(0,8) + " - ";
-            string textoPadrao = horaAtual + "Carta jogada: ";
-
-
-            switch (obj.GetType().Name)
-            {
-                case "Normal":
-                    Normal aux = (Normal)obj;
-                    listaEventos.Add(textoPadrao + aux.Cor + " " + aux.Numero);
-                    break;
-
-                case "Bot":
-                    Player bot = (Player)obj;
-                    listaEventos.Add(horaAtual + "Player atual: " + bot.Infos.Name);
-                    break;
-
-                case "Player":
-                    Player player = (Player)obj;
-                    listaEventos.Add(horaAtual + "Player atual: " + player.Infos.Name);
-                    break;
-
-                case "Especial":
-                    Especial especial = (Especial)obj;
-                    listaEventos.Add(textoPadrao + especial.Cor + ", Efeito: " + especial.Efeito);
-                    break;
-
-                case "Coringa":
-                    Coringa coringa = (Coringa)obj;
-                    listaEventos.Add(textoPadrao + coringa.Efeito);
-                    break;
-            }
+            
         }
     }
 }
